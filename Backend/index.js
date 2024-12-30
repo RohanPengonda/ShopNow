fdb30be9094914472a0ae624aad524732a64fff0
RohanPengonda <rpengonda1@gmail.com>
2024-12-30 14:31:51 +0530

    Updated

--- Backend/index.js
+++ Backend/index.js
@@ -38,7 +38,9 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
   res.json({
     success: 1,
-    image_url: `htttp://localhost:${port}/images/${req.file.filename}`
+
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+
   })
 })
 
@@ -78,8 +80,18 @@ const Product = mongoose.model("Product", {
 })
 
 app.post('/addproduct', async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  }
+  else {
+    id = 1
+  }
   const product = new Product({
-    id: req.body.id,
+    id: id,
     name: req.body.name,
     image: req.body.image,
     category: req.body.category,
@@ -94,6 +106,28 @@ app.post('/addproduct', async (req, res) => {
     name: req.body.name,
   })
 })
+
+// Creating API for deleting Products
+
+app.post('/removeproduct', async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({
+    success: true,
+    name: req.body.name
+
+  })
+})
+
+//Creating API for getting all Products
+app.get('/allproducts', async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products fetched");
+  res.send(products);
+
+})
+
+
 
 
 app.listen(port, (error) => {
